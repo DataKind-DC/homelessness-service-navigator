3a0ea8196f140f54b39016da91dc687f6e98ff41
chououtside <chouming3@gmail.com>
2017-11-22 11:04:39 -0500

    Project setup

---
+++ client/containers/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Homelessness Service Navigator</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
