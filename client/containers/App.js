ba375580584975bfa8563e7ac092e07f9ae88ef5
Alex Chou <chouming3@gmail.com>
2017-11-27 11:10:43 -0500

    Add js version of csv file

--- client/containers/App.js
+++ client/containers/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import data from '../data/Homeless_Services.js';
 
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Homelessness Service Navigator</h1>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<h1>Homelessness Service Navigator</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
