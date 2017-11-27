1cb84f4eff603a53c18760d07acff0176b103491
Alex Chou <chououtside@Alexs-MacBook-Pro.local>
2017-11-27 11:08:28 -0500

    Add csv data and add dsv loader to read file

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,10 @@ module.exports = {
         test: /\.jsx?$/,
         loader: 'babel-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.(c|d|t)sv$/, // load all .csv, .dsv, .tsv files with dsv-loader
+        use: ['dsv-loader'] // or dsv-loader?delimiter=,
       }
     ]
   }
