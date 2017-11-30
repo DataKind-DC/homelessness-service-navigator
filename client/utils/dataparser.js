f01087f97f529e95ffd65b3581f5f0940d586a1b
Alex Chou <chouming3@gmail.com>
2017-11-30 08:25:13 -0500

    write dataparser function to pass first unit test

--- client/utils/dataparser.js
+++ client/utils/dataparser.js
@@ -1,3 +1,28 @@
+import _ from 'lodash';
+import serviceKeys from '../data/serviceKeys';
+
 export default function(data) {
-	return data;
+	return _.map(data, row => {
+		let organizationName = row.ORGANIZATION_NAME;
+		let programName = row.PROGRAM_NAME;
+		let description = row.DESCRIPTION;
+		let website = row.WEBSITE_URL;
+		let address = `${row.STREET_ADDRESS}, ${row.CITY.trim()}, ${row.STATE} ${row.ZIP}`;
+		let services = [];
+
+		_.forEach(serviceKeys, key => {
+			if (row[key]) {
+				services.push(key);
+			}
+		});
+
+		return {
+			organizationName,
+			programName,
+			description,
+			website,
+			address,
+			services
+		};
+	});
 }
