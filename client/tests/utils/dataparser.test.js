b4e05999af2ca4b31d347f246f96b19872f564ac
Alex Chou <chouming3@gmail.com>
2017-11-30 08:25:13 -0500

    write dataparser function to pass first unit test

--- client/tests/utils/dataparser.test.js
+++ client/tests/utils/dataparser.test.js
@@ -17,7 +17,7 @@ describe('dataparser function should parse data to return a workable json object
 			'The Refugee Center is a home to welcome refugees and those who have asylum status to Washington, DC. The program helps these families to learn cultural norms about their new community in a safe environment and provides support in finding a job. Our staff understand what it is like to be refugee or asylee — many of them were once refugees themselves!  This allows us to offer services and support that fit the unique experiences of those who had to flee their home and come to the United States.\n In addition to our employment services, we offer refugees referrals for housing, help finding employment and education about the culture and social customs here in America.'
 		);
 		expect(firstElementInResult).toHaveProperty('website', 'https://www.catholiccharitiesdc.org/refugee/');
-		expect(firstElementInResult).toHaveProperty('address', '1018 MONROE STREET, NE, WASHINGTON, DC 20017');
+		expect(firstElementInResult).toHaveProperty('address', '1018 Monroe Street, NE, Washington, DC 20017');
 		expect(firstElementInResult).toHaveProperty('services', ['HOUSING_NAVIGATION', 'VOCATIONAL_TRAINING']);
 	});
 });
