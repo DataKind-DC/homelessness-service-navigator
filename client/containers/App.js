56913a04eddafa585da0e6bbe37d21c4e2f84a44
Alex Chou <chouming3@gmail.com>
2017-11-30 08:37:59 -0500

    Add favicon, and log parsed data

--- client/containers/App.js
+++ client/containers/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import data from '../data/Homeless_Services.js';
+import data from '../data/Homeless_Services';
+import dataParser from '../utils/dataparser';
 
 class App extends React.Component {
 	render() {
+		console.log(dataParser(data));
 		return (
 			<div>
 				<h1>Homelessness Service Navigator</h1>
