eff73bd97d6d6b4b57c8ef6f7b0232c5e1c00f1c
Huy Do <huy.do@wustl.edu>
2018-01-23 01:13:02 -0500

    add map

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Header } from './Header';
 import { Journey } from './Journey';
+import { Map } from './Map';
 
 class App extends Component {
   render() {
     return (
       <div>
+      <Map />
       <Header />
       <Journey />
       </div>
